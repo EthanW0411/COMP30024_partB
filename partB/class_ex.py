e2b2f2ec08881984131e20ec4fe2f1634d9e24ab
EthanW0411 <wlm454529340@gmail.com>
2018-04-30 16:21:08 +1000

    compistion in python ex

--- partB/class_ex.py
+++ partB/class_ex.py
@@ -6,15 +6,38 @@ class Employee:
         self.salary = salary
         Employee.empCount += 1
 
-    def displayCount(self):
+    def display_count(self):
         print("Total Employee %d" % Employee.empCount)
 
-    def displayEmployee(self):
+    def display_employee(self):
         print("Name: ", self.name, ", Salary: ", self.salary)
+
+    def __str__(self):
+        return "Name: %s, Salary: %s" % (self.name, self.salary)
+
+class Department():
+    def __init__(self, name):
+        self.__employees = []
+        self.name = name
+
+    def __str__(self):
+        return "Department Name: %s" % self.name
+
+    def display_employees(self):
+        for emp in self.__employees:
+            print(str(emp))
+
+    def build_department(self, employee):
+        self.__employees.append(employee)
 
 
 emp1 = Employee("Zara", 2000)
 emp2 = Employee("Jim", 5000)
 
-emp1.displayEmployee()
-emp2.displayEmployee()
+dep = Department("game dev")
+print(str(dep))
+
+dep.build_department(emp1)
+dep.build_department(emp2)
+dep.display_employees()
+
